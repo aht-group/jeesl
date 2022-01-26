cc6ffe604dcdf7c086a4748979e234e64aa0cec2
Helge Hemmer <hemmer@aht-group.com>
2022-01-26 07:51:37 +0100

    Added working version of markup pasting prevention to be included as JS file when pasting of markup should be prevented in PrimeFaces editors

--- jsf/src/main/resources/META-INF/resources/jeeslJs/preventMarkupPasting.js
+++ jsf/src/main/resources/META-INF/resources/jeeslJs/preventMarkupPasting.js
@@ -1,10 +1,11 @@
 // Prevents pasting of markup in contenteditable DIVs (e.g. PrimeFaces Editor)
-
-$("div[contenteditable='true']").each(function() {
-   $(this).bind('paste', function (e) {
-     e.preventDefault();
-     var content = (e.originalEvent || e).clipboardData.getData('Text');
-     console.log(content);
-     document.execCommand('insertText', false, content);
+window.addEventListener('load', function () {
+    $("div[contenteditable='true']").each(function() {
+       $(this).bind('paste', function (e) {
+	 e.preventDefault();
+	 var content = (e.originalEvent || e).clipboardData.getData('Text');
+	 console.log(content);
+	 document.execCommand('insertText', false, content);
+	});
     });
 });
