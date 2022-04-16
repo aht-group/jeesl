8ea1352d6efd3efae4726042bb4c753d7e8e2cc0
Thorsten-K <t.kisner@web.de>
2022-04-16 08:36:42 +0200

    JeeslDbEntityAttributeUpdater updated

--- util/src/main/java/org/jeesl/util/db/updater/JeeslDbEntityAttributeUpdater.java
+++ util/src/main/java/org/jeesl/util/db/updater/JeeslDbEntityAttributeUpdater.java
@@ -1,11 +1,12 @@
 package org.jeesl.util.db.updater;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
-import net.sf.ahtutils.xml.status.Lang;
+
 import org.jeesl.api.facade.io.JeeslIoRevisionFacade;
+import org.jeesl.exception.ejb.JeeslConstraintViolationException;
 import org.jeesl.factory.builder.io.IoRevisionFactoryBuilder;
+import org.jeesl.factory.ejb.system.status.EjbLangFactory;
 import org.jeesl.interfaces.model.io.revision.core.JeeslRevisionCategory;
 import org.jeesl.interfaces.model.io.revision.core.JeeslRevisionView;
 import org.jeesl.interfaces.model.io.revision.core.JeeslRevisionViewMapping;
@@ -19,13 +20,13 @@ import org.jeesl.interfaces.model.system.locale.JeeslLang;
 import org.jeesl.interfaces.model.system.locale.JeeslLocale;
 import org.jeesl.interfaces.model.system.locale.JeeslLocaleProvider;
 import org.jeesl.interfaces.model.system.locale.status.JeeslStatus;
+import org.jeesl.model.xml.system.revision.Attribute;
 import org.jeesl.model.xml.system.revision.Entity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.sf.ahtutils.xml.status.Lang;
 import net.sf.exlp.util.xml.JaxbUtil;
-import org.jeesl.model.xml.system.revision.Attribute;
-import org.jeesl.util.ReflectionUtil;
 
 public class JeeslDbEntityAttributeUpdater <L extends JeeslLang, D extends JeeslDescription, LOC extends JeeslLocale<L,D,LOC,?>,
 											RC extends JeeslRevisionCategory<L,D,RC,?>,
@@ -45,33 +46,35 @@ public class JeeslDbEntityAttributeUpdater <L extends JeeslLang, D extends Jeesl
 	private final IoRevisionFactoryBuilder<L,D,RC,RV,RVM,RS,RST,RE,REM,RA,RER,RAT,ERD,?> fbRevision;
 	private final JeeslIoRevisionFacade<L,D,RC,RV,RVM,RS,RST,RE,REM,RA,RER,RAT,ERD,?> fRevision;
 	
+	private final EjbLangFactory<L> efLang;
+	
 	public JeeslDbEntityAttributeUpdater(IoRevisionFactoryBuilder<L,D,RC,RV,RVM,RS,RST,RE,REM,RA,RER,RAT,ERD,?> fbRevision,
 									JeeslIoRevisionFacade<L,D,RC,RV,RVM,RS,RST,RE,REM,RA,RER,RAT,ERD,?> fRevision)
 	{
 		this.fbRevision=fbRevision;
 		this.fRevision=fRevision;
-	
+		
+		efLang = EjbLangFactory.factory(fbRevision.getClassL());
 	}
 	
-	public void updateAttributes(RE entity, JeeslLocaleProvider<LOC> lp, Entity xml) throws InstantiationException, IllegalAccessException, NoSuchFieldException, InvocationTargetException, NoSuchMethodException
+	public void updateAttributes(RE entity, JeeslLocaleProvider<LOC> lp, Entity xml) throws JeeslConstraintViolationException
 	{
 		logger.info("Creating/Updating Attributes of "+entity.toString()+ entity.getCode() +" for "+lp.getLocaleCodes());
 		JaxbUtil.info(xml);
 
 		// Init the lazy loading of EJB properties
-		Class<RE> target = (Class<RE>) entity.getClass();
-		entity = fRevision.load(target, entity);
+		entity = fRevision.load(fbRevision.getClassEntity(), entity);
 		
 		// Preparing Maps for storing the XML and EJB Attributes Objects referenced by their code
-		Map<String, Attribute> xmlAttributes				= new HashMap<>();
-		Map<String, JeeslRevisionAttribute> ejbAttributes	= new HashMap<>();
+		Map<String,Attribute> xmlAttributes				= new HashMap<>();
+		Map<String,RA> ejbAttributes	= new HashMap<>();
 
 		// Load data into the maps
 		for (Attribute attribute : xml.getAttribute())
 		{
 			xmlAttributes.put(attribute.getCode(), attribute);
 		}
-		for (JeeslRevisionAttribute ra : entity.getAttributes())
+		for (RA ra : entity.getAttributes())
 		{
 			ejbAttributes.put(ra.getCode(), ra);
 		}
@@ -81,9 +84,12 @@ public class JeeslDbEntityAttributeUpdater <L extends JeeslLang, D extends Jeesl
 		{
 			Attribute xmlAttribute = xmlAttributes.get(xmlCode);
 
+			RAT type = fRevision.all(fbRevision.getClassAttributeType(),1).get(0);
+			//Needs to be handled, check if type is part of the XML repsonse
+			
 			// See if an Attribute is available in the EJB entity already and load it
 			// Otherwise, use a new created instance
-			JeeslRevisionAttribute ejbAttribute = entity.getAttributes().get(0).getClass().newInstance();
+			RA ejbAttribute = fbRevision.ejbAttribute().build(type,xmlAttribute);
 			if (ejbAttributes.containsKey(xmlCode))
 			{
 				ejbAttribute = ejbAttributes.get(xmlCode);
@@ -96,40 +102,26 @@ public class JeeslDbEntityAttributeUpdater <L extends JeeslLang, D extends Jeesl
 				{
 					if (lang.getKey().equals(locale))
 					{	
-						JeeslLang ejbLang = (JeeslLang) ReflectionUtil.getTypeOfMapValues(ejbAttribute, "name").newInstance();
-						ejbLang.setLang(lang.getTranslation());
-						ejbLang.setLkey(locale);
-						ejbAttribute.getName().put(locale, ejbLang);
+						ejbAttribute.getName().put(locale, efLang.createLang(lang));
 					}
 				}
-				
 			}
-
-			// Set or Update all other properties of XML representation
-			ejbAttribute.setPosition(xmlAttribute.getPosition());
-			ejbAttribute.setXpath(xmlAttribute.getXpath());
-			ejbAttribute.setShowWeb(xmlAttribute.isWeb());
-			ejbAttribute.setShowPrint(xmlAttribute.isPrint());
-			ejbAttribute.setShowEnclosure(xmlAttribute.isEnclosure());
-			ejbAttribute.setUi(xmlAttribute.isUi());
-			ejbAttribute.setBean(xmlAttribute.isBean());
-			ejbAttribute.setConstruction(xmlAttribute.isConstruction());
-			ejbAttribute.setShowName(xmlAttribute.isName());
 
 			logger.info("EJB Attribute: " +ejbAttribute.toString());
 		}
 
 	}
 
-	public String findEnLang(Attribute xmlAttribute)
-	{
-		for (Lang lang : xmlAttribute.getLangs().getLang())
-		{
-			if (lang.getKey().equals("en"))
-			{
-				return lang.getTranslation();
-			}
-		}
-		return "no en translation available";
-	}
+	//Seems not required here ...
+//	public String findEnLang(Attribute xmlAttribute)
+//	{
+//		for (Lang lang : xmlAttribute.getLangs().getLang())
+//		{
+//			if (lang.getKey().equals("en"))
+//			{
+//				return lang.getTranslation();
+//			}
+//		}
+//		return "no en translation available";
+//	}
 }
