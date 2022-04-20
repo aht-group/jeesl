464fbe60de08cb58df156307cbcbbbde87778fa3
Helge Hemmer <hemmer@aht-group.com>
2022-04-20 21:14:41 +0200

    Added use of DB Updater for Lang objects in Attributes

--- util/src/main/java/org/jeesl/util/db/updater/JeeslDbEntityAttributeUpdater.java
+++ util/src/main/java/org/jeesl/util/db/updater/JeeslDbEntityAttributeUpdater.java
@@ -27,6 +27,8 @@ import org.slf4j.LoggerFactory;
 
 import net.sf.ahtutils.xml.status.Lang;
 import net.sf.exlp.util.xml.JaxbUtil;
+import org.jeesl.controller.db.updater.JeeslDbLangUpdater;
+import org.jeesl.exception.ejb.JeeslLockingException;
 
 public class JeeslDbEntityAttributeUpdater <L extends JeeslLang, D extends JeeslDescription, LOC extends JeeslLocale<L,D,LOC,?>,
 											RC extends JeeslRevisionCategory<L,D,RC,?>,
@@ -47,6 +49,8 @@ public class JeeslDbEntityAttributeUpdater <L extends JeeslLang, D extends Jeesl
 	private final JeeslIoRevisionFacade<L,D,RC,RV,RVM,RS,RST,RE,REM,RA,RER,RAT,ERD,?> fRevision;
 	
 	private final EjbLangFactory<L> efLang;
+	private final JeeslDbLangUpdater<RA,L> dbuLang;
+
 	
 	public JeeslDbEntityAttributeUpdater(IoRevisionFactoryBuilder<L,D,RC,RV,RVM,RS,RST,RE,REM,RA,RER,RAT,ERD,?> fbRevision,
 									JeeslIoRevisionFacade<L,D,RC,RV,RVM,RS,RST,RE,REM,RA,RER,RAT,ERD,?> fRevision)
@@ -55,6 +59,8 @@ public class JeeslDbEntityAttributeUpdater <L extends JeeslLang, D extends Jeesl
 		this.fRevision=fRevision;
 		
 		efLang = EjbLangFactory.factory(fbRevision.getClassL());
+		dbuLang = JeeslDbLangUpdater.factory(fbRevision.getClassAttribute(),fbRevision.getClassL());
+        
 	}
 	
 	public void updateAttributes(RE entity, JeeslLocaleProvider<LOC> lp, Entity xml) throws JeeslConstraintViolationException
@@ -68,6 +74,9 @@ public class JeeslDbEntityAttributeUpdater <L extends JeeslLang, D extends Jeesl
 		// Preparing Maps for storing the XML and EJB Attributes Objects referenced by their code
 		Map<String,Attribute> xmlAttributes				= new HashMap<>();
 		Map<String,RA> ejbAttributes	= new HashMap<>();
+
+		// Prepare the help object for Array conversion
+		String[] languageCodes = new String[lp.getLocaleCodes().size()];
 
 		// Load data into the maps
 		for (Attribute attribute : xml.getAttribute())
@@ -102,12 +111,21 @@ public class JeeslDbEntityAttributeUpdater <L extends JeeslLang, D extends Jeesl
 				{
 					if (lang.getKey().equals(locale))
 					{	
-						ejbAttribute.getName().put(locale, efLang.createLang(lang));
+						ejbAttribute.getName().put(locale, efLang.createLang(lang) );
 					}
 				}
 			}
-
-			logger.info("EJB Attribute: " +ejbAttribute.toString());
+			try {
+				efLang.persistMissingLangsForCode(fRevision, lp.getLocaleCodes(), entity);
+				dbuLang.handle(fRevision, ejbAttribute, lp.getLocaleCodes().toArray(languageCodes));
+				fRevision.save(fbRevision.getClassEntity(), entity, ejbAttribute); 
+				fRevision.save(entity);
+				
+				logger.info("EJB Attribute: " +ejbAttribute.toString() +" has been updated in database");
+			} catch(JeeslLockingException e)
+			{
+				logger.error("EJB Attribute: " +ejbAttribute.toString() +" has not been updated in database. Reason: " +e.getMessage());
+			}
 		}
 
 	}
