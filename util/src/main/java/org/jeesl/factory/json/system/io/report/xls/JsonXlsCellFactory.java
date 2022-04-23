a0f6fcdc2221a351be45aacc56ae68f4400d8cd5
Thorsten-K <t.kisner@web.de>
2022-04-23 08:39:43 +0100

    updated to enum approach

--- util/src/main/java/org/jeesl/factory/json/system/io/report/xls/JsonXlsCellFactory.java
+++ util/src/main/java/org/jeesl/factory/json/system/io/report/xls/JsonXlsCellFactory.java
@@ -19,27 +19,27 @@ public class JsonXlsCellFactory
 		JsonXlsCell json = build();
 		
 		//TODO tk change to enum approach
-		int type = cell.getCellType();
-		if(type==1) {json.setValueString(cell.getStringCellValue());}
-		else if(type==0) {json.setValueString(cell.getStringCellValue());}
-		else if(type==3) {/*blank*/}
-		else
-		{
-			logger.error("You need to handle type="+type+" in row:"+cell.getRowIndex()+" cell:"+cell.getColumnIndex());
-			logger.error(cell.getStringCellValue());
-			System.exit(-1);
-		}
+//		int type = cell.getCellType();
+//		if(type==1) {json.setValueString(cell.getStringCellValue());}
+//		else if(type==0) {json.setValueString(cell.getStringCellValue());}
+//		else if(type==3) {/*blank*/}
+//		else
+//		{
+//			logger.error("You need to handle type="+type+" in row:"+cell.getRowIndex()+" cell:"+cell.getColumnIndex());
+//			logger.error(cell.getStringCellValue());
+//			System.exit(-1);
+//		}
 		
 		//enum does not work with streaming-api: java.lang.AbstractMethodError: com.monitorjbl.xlsx.impl.StreamingCell.getCellTypeEnum()Lorg/apache/poi/ss/usermodel/CellType;
-//		CellType type = cell.getCellTypeEnum();
-//		switch(type)
-//		{
-//			case NUMERIC: json.setValueString(cell.getStringCellValue()); break;
-//			case STRING: json.setValueString(cell.getStringCellValue()); break;
-//			case BLANK: break;
-//			default: logger.error("You need to handle "+type);System.exit(-1);
-//		}
-//		
+		CellType type = cell.getCellTypeEnum();
+		switch(type)
+		{
+			case NUMERIC: json.setValueString(cell.getStringCellValue()); break;
+			case STRING: json.setValueString(cell.getStringCellValue()); break;
+			case BLANK: break;
+			default: logger.error("You need to handle "+type);System.exit(-1);
+		}
+		
 		return json;
 	}
 	
