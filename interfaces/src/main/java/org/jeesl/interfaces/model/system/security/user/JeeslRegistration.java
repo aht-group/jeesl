54bc7982e3b0794605885b024473c449ccf68591
Thorsten-K <t.kisner@web.de>
2022-04-27 07:46:38 +0100

    GridConstraint improved

--- interfaces/src/main/java/org/jeesl/interfaces/model/system/security/user/JeeslRegistration.java
+++ interfaces/src/main/java/org/jeesl/interfaces/model/system/security/user/JeeslRegistration.java
@@ -1,5 +1,7 @@
 package org.jeesl.interfaces.model.system.security.user;
 
+import java.io.Serializable;
+
 import org.jeesl.interfaces.model.marker.jpa.EjbRemoveable;
 import org.jeesl.interfaces.model.marker.jpa.EjbSaveable;
 import org.jeesl.interfaces.model.system.locale.JeeslDescription;
@@ -10,7 +12,7 @@ import org.jeesl.interfaces.model.with.primitive.number.EjbWithId;
 public interface JeeslRegistration <L extends JeeslLang, D extends JeeslDescription,
 							USER extends JeeslUser<?>,
 							REGSTATUS extends JeeslStatus<L,D,REGSTATUS>>
-		extends EjbWithId,EjbSaveable,EjbRemoveable
+		extends Serializable,EjbWithId,EjbSaveable,EjbRemoveable
 {	
 	USER getUser();
 	void setUser(USER user);
