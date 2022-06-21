13eaaa3510656e9b6365d2f256efe2f211b95966
Sachin.Manandhar <Sachin.Manandhar@nbsm19.mshome.net>
2022-06-21 19:44:35 +0200

    activity monitoring improved

--- util/src/main/java/org/jeesl/factory/ejb/module/am/EjbActivityFactory.java
+++ util/src/main/java/org/jeesl/factory/ejb/module/am/EjbActivityFactory.java
@@ -1,5 +1,7 @@
 package org.jeesl.factory.ejb.module.am;
 
+import java.util.UUID;
+
 import org.jeesl.interfaces.model.module.am.JeesAmProject;
 import org.jeesl.interfaces.model.module.am.JeeslAmActivity;
 import org.jeesl.interfaces.model.system.tenant.JeeslTenantRealm;
@@ -21,11 +23,14 @@ public class EjbActivityFactory<REALM extends JeeslTenantRealm<?,?,REALM,?>,
 
 	public ACTIVITY build(PROJ project, ACTIVITY parent)
 	{
+		UUID uuid = UUID.randomUUID();
+        String uuidAsString = uuid.toString();
 		try
 		{
 			ACTIVITY ejb = cType.newInstance();
 			ejb.setProject(project);
 			ejb.setParent(parent);
+			ejb.setCode(uuidAsString);
 		    return ejb;
 		}
 		catch (InstantiationException e) {e.printStackTrace();}
