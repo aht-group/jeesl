148ba063e062d7722f05de91e6424e433e33a496
Thorsten-K <t.kisner@web.de>
2022-07-07 11:47:54 +0200

    Zone added to Calendar

--- interfaces/src/main/java/org/jeesl/interfaces/model/module/calendar/JeeslCalendar.java
+++ interfaces/src/main/java/org/jeesl/interfaces/model/module/calendar/JeeslCalendar.java
@@ -2,6 +2,7 @@ package org.jeesl.interfaces.model.module.calendar;
 
 import java.io.Serializable;
 
+import org.jeesl.interfaces.model.marker.jpa.EjbRemoveable;
 import org.jeesl.interfaces.model.marker.jpa.EjbSaveable;
 import org.jeesl.interfaces.model.with.primitive.number.EjbWithId;
 import org.jeesl.interfaces.model.with.system.status.JeeslWithType;
@@ -10,7 +11,11 @@ public interface JeeslCalendar <
 								ZONE extends JeeslCalendarTimeZone<?,?>,
 								CT extends JeeslCalendarType<?,?,CT,?>
 								>
-		extends Serializable,EjbWithId,EjbSaveable,JeeslWithType<CT>
+		extends Serializable,EjbWithId,EjbSaveable,EjbRemoveable,
+					JeeslWithType<CT>
 {
 //	void test();
+	
+	ZONE getZone();
+	void setZone(ZONE zone);
 }
