f494b36f7e2bc8d2178559bb2ab2b45ae5a1f85d
Thorsten-K <t.kisner@web.de>
2022-10-26 07:26:33 +0200

    Test for native image compilation

---
+++ client/src/main/java/org/jeesl/client/app/JeeslTest.java
@@ -0,0 +1,14 @@
+package org.jeesl.client.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JeeslTest
+{
+	final static Logger logger = LoggerFactory.getLogger(JeeslTest.class);
+
+	public static void main(String args[])
+	{
+		System.out.println("Hello World");
+	}
+}
