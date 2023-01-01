685fb2e252ff37f1e49b78636027da3ac561ea58
Thorsten-K <t.kisner@web.de>
2023-01-01 16:09:54 +0100

    SecurityUser added

---
+++ system-security/src/main/java/org/jeesl/model/ejb/system/security/user/SecurityUser.java
@@ -0,0 +1,43 @@
+package org.jeesl.model.ejb.system.security.user;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+
+import org.jeesl.interfaces.model.with.primitive.number.EjbWithId;
+import org.jeesl.interfaces.model.with.primitive.text.EjbWithEmail;
+
+@Entity
+@Table(name="SecurityUser",uniqueConstraints=@UniqueConstraint(columnNames={"email"}))
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="system", discriminatorType=DiscriminatorType.STRING, length=32)
+@DiscriminatorValue("generic")
+@SequenceGenerator(name="SequenceUser", sequenceName="SecurityUser_id_seq", allocationSize=1)
+public abstract class SecurityUser  implements EjbWithId,EjbWithEmail
+{
+	public static final long serialVersionUID=1;
+	
+	@Id @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="SequenceUser")
+	protected long id;
+	@Override public long getId() {return id;}
+	@Override public void setId(long id) {this.id = id;}
+	
+	@NotNull @Column(unique=true)
+	protected String email;
+	@Override public String getEmail() {return email;}
+	@Override public void setEmail(String email) {this.email = email;}
+	
+//	public abstract void test();
+
+}
