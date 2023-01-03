bfbdaf0e203dbc2d0ccf4f2f964b1cf3c9273055
Thorsten-K <t.kisner@web.de>
2023-01-03 14:11:07 +0100

    files added to io-fr

---
+++ io-fr/src/main/java/org/jeesl/model/ejb/io/fr/IoFileReplicationType.java
@@ -0,0 +1,33 @@
+package org.jeesl.model.ejb.io.fr;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.jeesl.interfaces.model.io.fr.JeeslFileReplicationType;
+import org.jeesl.interfaces.qualifier.er.EjbErNode;
+import org.jeesl.model.ejb.io.graphic.core.IoGraphic;
+import org.jeesl.model.ejb.io.locale.IoDescription;
+import org.jeesl.model.ejb.io.locale.IoLang;
+import org.jeesl.model.ejb.io.locale.IoStatus;
+
+@Entity
+@DiscriminatorValue("ioFrRT")
+@EjbErNode(name="Category",category="system",subset="fr",level=3)
+public class IoFileReplicationType extends IoStatus implements JeeslFileReplicationType<IoLang,IoDescription,IoFileReplicationType,IoGraphic>
+{
+	public static final long serialVersionUID=1;
+	
+	@Override public List<String> getFixedCodes()
+	{
+		List<String> fixed = new ArrayList<String>();
+		for(JeeslFileReplicationType.Code c : JeeslFileReplicationType.Code.values()){fixed.add(c.toString());}
+		return fixed;
+	}
+	
+	@Override public boolean equals(Object object) {return (object instanceof IoFileReplicationType) ? id == ((IoFileReplicationType) object).getId() : (object == this);}
+	@Override public int hashCode(){return new HashCodeBuilder(17,37).append(id).toHashCode();}
+}
