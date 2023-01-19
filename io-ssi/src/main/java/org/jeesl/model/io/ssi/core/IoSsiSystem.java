aed8ad4236fd11717d9842cdb1849b3c9a35bd55
Thorsten-K <t.kisner@web.de>
2023-01-19 12:11:44 +0200

    IoSsiSystem added

---
+++ io-ssi/src/main/java/org/jeesl/model/io/ssi/core/IoSsiSystem.java
@@ -0,0 +1,72 @@
+package org.jeesl.model.io.ssi.core;
+
+import java.util.Map;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.MapKey;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.jeesl.interfaces.model.io.ssi.core.JeeslIoSsiSystem;
+import org.jeesl.interfaces.qualifier.er.EjbErNode;
+import org.jeesl.model.ejb.io.locale.IoDescription;
+import org.jeesl.model.ejb.io.locale.IoLang;
+
+@Entity
+@Table(name="IoSsiSystem",uniqueConstraints={@UniqueConstraint(columnNames={"code"})})
+@EjbErNode(name="System")
+public class IoSsiSystem implements JeeslIoSsiSystem<IoLang,IoDescription>
+{
+	public static final long serialVersionUID=1;
+
+
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	@Override public long getId() {return id;}
+	@Override public void setId(long id) {this.id = id;}
+
+	@NotNull
+	private String code;
+	@Override public String getCode() {return code;}
+	@Override public void setCode(String code) {this.code = code;}
+	
+	private String fqdn;
+	@Override public String getFqdn() {return fqdn;}
+	@Override public void setFqdn(String fqdn) {this.fqdn = fqdn;}
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch=FetchType.EAGER)
+	@MapKey(name="lkey")
+	@JoinTable(name="IoSsiSystemJtLang",joinColumns={@JoinColumn(name="system_id")},inverseJoinColumns={@JoinColumn(name="lang_id")})
+	private Map<String,IoLang> name;
+	@Override public Map<String,IoLang> getName() {return name;}
+	@Override public void setName(Map<String,IoLang> name) {this.name = name;}
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch=FetchType.EAGER)
+	@MapKey(name="lkey")
+	@JoinTable(name="IoSsiSystemJtDescription",joinColumns={@JoinColumn(name="system_id")},inverseJoinColumns={@JoinColumn(name="description_id")})
+	private Map<String,IoDescription> description;
+	@Override public Map<String,IoDescription> getDescription() {return description;}
+	@Override public void setDescription(Map<String,IoDescription> description) {this.description = description;}
+
+
+	@Override public boolean equals(Object object){return (object instanceof IoSsiSystem) ? id == ((IoSsiSystem) object).getId() : (object == this);}
+	@Override public int hashCode() {return new HashCodeBuilder(17,57).append(id).toHashCode();}
+	
+	@Override public String toString()
+	{
+		StringBuilder sb = new StringBuilder();
+		sb.append("[").append(id).append("]");
+		sb.append(" code:").append(code);
+		return sb.toString();
+	}
+}
