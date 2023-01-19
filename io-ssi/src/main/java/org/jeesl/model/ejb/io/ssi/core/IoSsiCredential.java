e63e3078f1ab0447e7ef396768172df847b83c99
Thorsten-K <t.kisner@web.de>
2023-01-19 19:55:34 +0200

    IoSsiCredential moved to jeesl

---
+++ io-ssi/src/main/java/org/jeesl/model/ejb/io/ssi/core/IoSsiCredential.java
@@ -0,0 +1,88 @@
+package org.jeesl.model.ejb.io.ssi.core;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.jeesl.interfaces.model.io.ssi.core.JeeslIoSsiCredential;
+import org.jeesl.interfaces.qualifier.er.EjbErNode;
+
+@Entity
+@Table(name="IoSsiCredential",uniqueConstraints={@UniqueConstraint(name="UK_IoSsiCredential_system_code",columnNames={"system_id","code"})})
+@EjbErNode(name="Credential")
+public class IoSsiCredential implements JeeslIoSsiCredential<IoSsiSystem>
+{
+	public static final long serialVersionUID=1;
+
+
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	@Override public long getId() {return id;}
+	@Override public void setId(long id) {this.id = id;}
+
+	@Override public String resolveParentAttribute() {return JeeslIoSsiCredential.Attributes.system.toString();}
+	@NotNull @ManyToOne
+	private IoSsiSystem system;
+	@Override public IoSsiSystem getSystem(){return system;}
+	@Override public void setSystem(IoSsiSystem system){this.system = system;}
+
+	@NotNull
+	private String code;
+	@Override public String getCode() {return code;}
+	@Override public void setCode(String code) {this.code = code;}
+
+	private int position;
+	@Override public int getPosition() {return position;}
+	@Override public void setPosition(int position) {this.position = position;}
+
+	private Boolean visible;
+	@Override public Boolean getVisible() {return visible;}
+	@Override public void setVisible(Boolean visible) {this.visible = visible;}
+
+	@Column(name="username")
+	private String user;
+	@Override public String getUser() {return user;}
+	@Override public void setUser(String user) {this.user = user;}
+
+	private String pwd;
+	@Override public String getPwd() {return pwd;}
+	@Override public void setPwd(String pwd) {this.pwd = pwd;}
+
+	private String token;
+	@Override public String getToken() {return token;}
+	@Override public void setToken(String token) {this.token = token;}
+
+	private String url;
+	@Override public String getUrl() {return url;}
+	@Override public void setUrl(String url) {this.url = url;}
+
+	private String host;
+	@Override public String getHost() {return host;}
+	@Override public void setHost(String host) {this.host = host;}
+
+	private Integer port;
+	@Override public Integer getPort() {return port;}
+	@Override public void setPort(Integer port) {this.port = port;}
+
+	private boolean encrypted;
+	@Override public boolean isEncrypted() {return encrypted;}
+	@Override public void setEncrypted(boolean encrypted) {this.encrypted = encrypted;}
+
+
+	@Override public boolean equals(Object object){return (object instanceof IoSsiCredential) ? id == ((IoSsiCredential) object).getId() : (object == this);}
+	@Override public int hashCode() {return new HashCodeBuilder(17,53).append(id).toHashCode();}
+
+	@Override public String toString()
+	{
+		StringBuffer sb = new StringBuffer();
+		sb.append("[").append(id).append("]");
+		return sb.toString();
+	}
+}
