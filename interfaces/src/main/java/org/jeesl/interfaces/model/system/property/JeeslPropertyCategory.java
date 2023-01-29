5be038633aa9a44e91bfae9eed91b6edc32f3940
Thorsten-K <t.kisner@web.de>
2023-01-29 06:30:35 +0100

    system-property added

--- interfaces/src/main/java/org/jeesl/interfaces/model/system/property/JeeslPropertyCategory.java
+++ interfaces/src/main/java/org/jeesl/interfaces/model/system/property/JeeslPropertyCategory.java
@@ -7,13 +7,12 @@ import org.jeesl.interfaces.model.system.graphic.core.JeeslGraphic;
 import org.jeesl.interfaces.model.system.graphic.with.EjbWithCodeGraphic;
 import org.jeesl.interfaces.model.system.locale.JeeslDescription;
 import org.jeesl.interfaces.model.system.locale.JeeslLang;
-import org.jeesl.interfaces.model.system.locale.status.JeeslStatusFixedCode;
 import org.jeesl.interfaces.model.system.locale.status.JeeslStatus;
 
 public interface JeeslPropertyCategory <L extends JeeslLang, D extends JeeslDescription,
 								S extends JeeslStatus<L,D,S>,
 								G extends JeeslGraphic<?,?,?>>
-						extends Serializable,EjbSaveable,JeeslStatusFixedCode,
+						extends Serializable,EjbSaveable,
 								EjbWithCodeGraphic<G>,
 								JeeslStatus<L,D,S>
 {
