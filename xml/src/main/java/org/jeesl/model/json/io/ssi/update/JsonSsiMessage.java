af5e4895942d4453adc1fdcf2f9684b7ce15f6e3
Thorsten-K <t.kisner@web.de>
2023-03-01 09:13:50 +0100

    formatting improved

--- xml/src/main/java/org/jeesl/model/json/io/ssi/update/JsonSsiMessage.java
+++ xml/src/main/java/org/jeesl/model/json/io/ssi/update/JsonSsiMessage.java
@@ -17,13 +17,7 @@ public class JsonSsiMessage implements Serializable
 
 	@JsonProperty("message")
 	private String message;
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
+	public String getMessage() {return message;}
+	public void setMessage(String message) {this.message = message;}
 	
 }
