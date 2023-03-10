9d710b5bf10aab4b3a3fcb3800e8623fdce0d1cf
Thorsten-K <t.kisner@web.de>
2023-03-10 10:48:59 +0100

    markup added

---
+++ io-cms/src/main/java/org/jeesl/model/ejb/io/cms/markup/IoMarkup.java
@@ -0,0 +1,50 @@
+package org.jeesl.model.ejb.io.cms.markup;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.jeesl.interfaces.model.system.locale.JeeslMarkup;
+
+@Entity
+@Table(name="LocaleMarkup")
+public class IoMarkup implements JeeslMarkup<IoMarkupType>
+{
+	public static final long serialVersionUID=1;
+
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	@Override public long getId() {return id;}
+	@Override public void setId(long id) {this.id = id;}
+	
+	@NotNull
+	private String lkey;
+	@Override public String getLkey() {return lkey;}
+	@Override public void setLkey(String lkey) {this.lkey = lkey;}
+	
+	@NotNull @Basic @Column(columnDefinition="text")
+	private String content;
+	@Override public String getContent() {return content;}
+	@Override public void setContent(String content) {this.content = content;}
+
+	@NotNull @ManyToOne
+	private IoMarkupType type;
+	@Override public IoMarkupType getType() {return type;}
+	@Override public void setType(IoMarkupType type) {this.type = type;}
+	
+	@Override public String toString()
+	{
+		StringBuilder sb = new StringBuilder();
+		sb.append("[").append(id).append("]");
+		sb.append(" ").append(lkey);
+		sb.append("-").append(type.getCode());
+		sb.append(": ").append(content);
+		return sb.toString();
+	}
+}
---
+++ io-cms/src/main/java/org/jeesl/model/ejb/io/cms/markup/IoMarkupType.java
@@ -0,0 +1,33 @@
+package org.jeesl.model.ejb.io.cms.markup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.jeesl.interfaces.model.io.cms.JeeslIoCmsMarkupType;
+import org.jeesl.interfaces.qualifier.er.EjbErNode;
+import org.jeesl.model.ejb.io.locale.IoLang;
+import org.jeesl.model.ejb.io.locale.IoStatus;
+import org.jeesl.model.ejb.io.graphic.core.IoGraphic;
+import org.jeesl.model.ejb.io.locale.IoDescription;
+
+@Entity
+@DiscriminatorValue("ioCmsMarkupType")
+@EjbErNode(name="Type",category="system",subset="mail",level=3)
+public class IoMarkupType extends IoStatus implements JeeslIoCmsMarkupType<IoLang,IoDescription,IoMarkupType,IoGraphic>
+{
+	public static final long serialVersionUID=1;
+
+	@Override public List<String> getFixedCodes()
+	{
+		List<String> fixed = new ArrayList<>();
+		for(JeeslIoCmsMarkupType.Code c : JeeslIoCmsMarkupType.Code.values()){fixed.add(c.toString());}
+		return fixed;
+	}
+	
+	@Override public boolean equals(Object object) {return (object instanceof IoMarkupType) ? id == ((IoMarkupType) object).getId() : (object == this);}
+	@Override public int hashCode(){return new HashCodeBuilder(17,37).append(id).toHashCode();}
+}
