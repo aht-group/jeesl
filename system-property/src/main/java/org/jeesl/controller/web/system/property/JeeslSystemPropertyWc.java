98b0bd0601a6180881bfc3072f5c6753656e1dc7
Thorsten-K <t.kisner@web.de>
2023-03-24 07:13:01 +0200

    JeeslSystemPropertyWc added

--- system-property/src/main/java/org/jeesl/controller/web/system/property/JeeslSystemPropertyWc.java
+++ system-property/src/main/java/org/jeesl/controller/web/system/property/JeeslSystemPropertyWc.java
@@ -1,6 +1,19 @@
 package org.jeesl.controller.web.system.property;
 
-public class JeeslSystemPropertyWc //extends JeeslSystemPropertyGc<IoLang,IoDescription,IoLocale,SystemPropertyCategory,SystemProperty>
-{
+import org.jeesl.controller.web.system.JeeslSystemPropertyGwc;
+import org.jeesl.factory.builder.system.PropertyFactoryBuilder;
+import org.jeesl.model.ejb.io.locale.IoDescription;
+import org.jeesl.model.ejb.io.locale.IoLang;
+import org.jeesl.model.ejb.io.locale.IoLocale;
+import org.jeesl.model.ejb.system.property.SystemProperty;
+import org.jeesl.model.ejb.system.property.SystemPropertyCategory;
 
+public class JeeslSystemPropertyWc extends JeeslSystemPropertyGwc<IoLang,IoDescription,IoLocale,SystemPropertyCategory,SystemProperty>
+{
+	private static final long serialVersionUID = 1L;
+
+	public JeeslSystemPropertyWc(final PropertyFactoryBuilder<IoLang,IoDescription,SystemPropertyCategory,SystemProperty> fbProperty)
+	{
+		super(fbProperty);
+	}
 }
