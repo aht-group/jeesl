1a717ef5b0c50279a7cf41d1699b7adca5a83c0e
Thorsten-K <t.kisner@web.de>
2023-05-19 06:11:34 +0200

    tuple handler for <A> fixed

--- interfaces/src/main/java/org/jeesl/exception/processing/UtilsProcessingException.java
+++ interfaces/src/main/java/org/jeesl/exception/processing/UtilsProcessingException.java
@@ -6,12 +6,24 @@ public class UtilsProcessingException extends Exception implements Serializable
 {
 	private static final long serialVersionUID = 1;
 
+	private String code;
+	
+	public String getCode() {
+		return code;
+	}
+
+	public static UtilsProcessingException instance(String message) {return new UtilsProcessingException(message);}
+	
 	public UtilsProcessingException() 
 	{ 
-	} 
+
+	}
  
 	public UtilsProcessingException(String s) 
 	{ 
 		super(s); 
-	} 
+	}
+	
+	public UtilsProcessingException code(String code) {this.code=code; return this;}
+//	public UtilsProcessingException message(String message) {
 }
