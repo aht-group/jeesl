a6230a6c662ff198213db51c0f94bcdd3e706268
Helge Hemmer <hhemmer@web.de>
2023-05-24 19:38:49 +0200

    Removed CKEditor configuration from wrong location

--- editor-config.js
+++
@@ -1,7 +0,0 @@
-CKEDITOR.config.forcePasteAsPlainText = true
-CKEDITOR.config.pasteFromWordRemoveStyles = true
-CKEDITOR.config.pasteFromWordRemoveFontStyles = true
-
-config.forcePasteAsPlainText = true
-config.pasteFromWordRemoveStyles = true
-config.pasteFromWordRemoveFontStyles = true
