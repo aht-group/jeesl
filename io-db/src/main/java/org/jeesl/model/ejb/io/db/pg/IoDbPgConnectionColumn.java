899f837eccbcbbc9076edb85554e9f148c84ffe1
Thorsten-K <t.kisner@web.de>
2023-07-14 14:42:52 +0200

    IoDbPgConnectionColumn added

---
+++ io-db/src/main/java/org/jeesl/model/ejb/io/db/pg/IoDbPgConnectionColumn.java
@@ -0,0 +1,33 @@
+package org.jeesl.model.ejb.io.db.pg;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.jeesl.interfaces.model.io.db.pg.JeeslDbConnectionColumn;
+import org.jeesl.interfaces.qualifier.er.EjbErNode;
+import org.jeesl.model.ejb.io.graphic.core.IoGraphic;
+import org.jeesl.model.ejb.io.locale.IoDescription;
+import org.jeesl.model.ejb.io.locale.IoLang;
+import org.jeesl.model.ejb.io.locale.IoStatus;
+
+@Entity
+@DiscriminatorValue("ioDbConnectionColumn")
+@EjbErNode(name="Connection Column",category="system",subset="db",level=3)
+public class IoDbPgConnectionColumn extends IoStatus implements JeeslDbConnectionColumn<IoLang,IoDescription,IoDbPgConnectionColumn,IoGraphic>
+{
+	public static final long serialVersionUID=1;
+	
+	@Override public List<String> getFixedCodes()
+	{
+		List<String> fixed = new ArrayList<String>();
+		for(JeeslDbConnectionColumn.Code c : JeeslDbConnectionColumn.Code.values()){fixed.add(c.toString());}
+		return fixed;
+	}
+	
+	@Override public boolean equals(Object object) {return (object instanceof IoDbPgConnectionColumn) ? id == ((IoDbPgConnectionColumn) object).getId() : (object == this);}
+	@Override public int hashCode(){return new HashCodeBuilder(17,37).append(id).toHashCode();}
+}
