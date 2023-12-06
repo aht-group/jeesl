2a85d69aad028319c7806a951700000b24b561b8
Thorsten-K <t.kisner@web.de>
2023-12-06 11:37:28 +0100

    MenuCache now independent of implementation (Map,Caffeine,Infinispan)

--- api/src/main/java/org/jeesl/api/bean/cache/JeeslMenuCache.java
+++ api/src/main/java/org/jeesl/api/bean/cache/JeeslMenuCache.java
@@ -3,13 +3,16 @@ package org.jeesl.api.bean.cache;
 import java.io.Serializable;
 import java.util.List;
 
-import org.infinispan.Cache;
 import org.jeesl.interfaces.model.system.security.context.JeeslSecurityMenu;
 
 public interface JeeslMenuCache <M extends JeeslSecurityMenu<?,?,?,M>>
 							extends Serializable
 {	
-	Cache<String,List<M>> cacheMenu();
-	Cache<String,List<M>> cacheCrumb();
-	Cache<String,List<M>> cacheSub();
+//	Cache<String,List<M>> cacheMenu();
+//	Cache<String,List<M>> cacheCrumb();
+//	Cache<String,List<M>> cacheSub();
+	
+	boolean cacheContains(String key);
+	List<M> cacheGet(String key);
+	void cachePut(String key, List<M> list);
 }
