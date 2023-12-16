16f86d637a7015b7cc6c8f183f3d62c10cc5ae21
Thorsten-K <t.kisner@web.de>
2023-12-16 11:11:34 +0100

    baseline data added

---
+++ io-label/src/main/resources/jeesl/system/io/db/migration/io/label/0-Baseline Data.sql
@@ -0,0 +1,16 @@
+INSERT INTO IoLabelEntity (position,code,jscn,visible,timeseries,documentation) VALUES (1,'org.jeesl.model.ejb.io.label.entity.IoLabelAttribute','IoLabelAttribute','t','f','f');
+INSERT INTO IoLabelEntity (position,code,jscn,visible,timeseries,documentation) VALUES (1,'org.jeesl.model.ejb.io.label.entity.IoLabelAttributeType','IoLabelAttributeType','t','f','f');
+INSERT INTO IoLabelEntity (position,code,jscn,visible,timeseries,documentation) VALUES (1,'org.jeesl.model.ejb.io.label.entity.IoLabelCategory','IoLabelCategory','t','f','f');
+INSERT INTO IoLabelEntity (position,code,jscn,visible,timeseries,documentation) VALUES (1,'org.jeesl.model.ejb.io.label.entity.IoLabelEntity','IoLabelEntity','t','f','f');
+INSERT INTO IoLabelEntity (position,code,jscn,visible,timeseries,documentation) VALUES (1,'org.jeesl.model.ejb.io.label.entity.IoLabelMissing','IoLabelMissing','t','f','f');
+
+INSERT INTO IoLabelEntity (position,code,jscn,visible,timeseries,documentation) VALUES (1,'org.jeesl.model.ejb.io.label.er.IoLabelDiagram','IoLabelDiagram','t','f','f');
+INSERT INTO IoLabelEntity (position,code,jscn,visible,timeseries,documentation) VALUES (1,'org.jeesl.model.ejb.io.label.er.IoLabelEntityRelation','IoLabelEntityRelation','t','f','f');
+
+INSERT INTO IoLabelEntity (position,code,jscn,visible,timeseries,documentation) VALUES (1,'org.jeesl.model.ejb.io.label.revision.IoRevisionEntityMapping','IoRevisionEntityMapping','t','f','f');
+INSERT INTO IoLabelEntity (position,code,jscn,visible,timeseries,documentation) VALUES (1,'org.jeesl.model.ejb.io.label.revision.IoRevisionScope','IoRevisionScope','t','f','f');
+INSERT INTO IoLabelEntity (position,code,jscn,visible,timeseries,documentation) VALUES (1,'org.jeesl.model.ejb.io.label.revision.IoRevisionScopeType','IoRevisionScopeType','t','f','f');
+INSERT INTO IoLabelEntity (position,code,jscn,visible,timeseries,documentation) VALUES (1,'org.jeesl.model.ejb.io.label.revision.IoRevisionView','IoRevisionView','t','f','f');
+INSERT INTO IoLabelEntity (position,code,jscn,visible,timeseries,documentation) VALUES (1,'org.jeesl.model.ejb.io.label.revision.IoRevisionViewMapping','IoRevisionViewMapping','t','f','f');
+
+INSERT INTO IoLabelEntity (position,code,jscn,visible,timeseries,documentation) VALUES (1,'org.jeesl.model.ejb.io.label.ui.IoLabelNumberPrecision','IoLabelNumberPrecision','t','f','f');
