04ba21f104c3c8b2515d402787f8577e367f3b06
Thorsten-K <t.kisner@web.de>
2023-12-18 11:16:00 +0100

    xml:net/text moved to javax

--- xml/src/main/javax/org/jeesl/model/xml/io/cms/text/ObjectFactory.java
+++ xml/src/main/javax/org/jeesl/model/xml/io/cms/text/ObjectFactory.java
@@ -149,4 +149,12 @@ public class ObjectFactory {
         return new Reference();
     }
 
+    /**
+     * Create an instance of {@link Title }
+     * 
+     */
+    public Title createTitle() {
+        return new Title();
+    }
+
 }
