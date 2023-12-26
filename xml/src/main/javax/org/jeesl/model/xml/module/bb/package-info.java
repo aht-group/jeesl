b576c89dd46b8a40802c047124130ab7aa997a1f
Thorsten-K <t.kisner@web.de>
2023-12-26 08:03:46 +0100

    namespace of bb changed

--- xml/src/main/javax/org/jeesl/model/xml/module/bb/package-info.java
+++ xml/src/main/javax/org/jeesl/model/xml/module/bb/package-info.java
@@ -1,2 +1,2 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.jeesl.org/bb", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://xsd.jeesl.org/module/bb", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package org.jeesl.model.xml.module.bb;
