b578b4f9833995fb87a42956b1411b45e708bce7
Thorsten-K <t.kisner@web.de>
2024-03-11 12:00:34 +0200

    added method to build remarks with EjbWithCode

--- util/src/main/java/org/jeesl/factory/xml/system/util/text/XmlRemarkFactory.java
+++ util/src/main/java/org/jeesl/factory/xml/system/util/text/XmlRemarkFactory.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 import org.jeesl.interfaces.model.system.locale.JeeslLang;
+import org.jeesl.interfaces.model.with.primitive.code.EjbWithCode;
 import org.jeesl.model.xml.io.cms.text.Remark;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,7 +32,8 @@ public class XmlRemarkFactory<L extends JeeslLang>
 	
 	public static Remark build(String value) {return build(null,null,value);}
 	
-	public static <E extends Enum<E>> Remark build(E key, String value){return build(key.toString(),value);}
+	public static <E extends Enum<E>> Remark build(E key, String value) {return build(key.toString(),value);}
+	public static <C extends EjbWithCode> Remark build(C code, String value) {return build(code.getCode(),value);}
 	public static Remark build(String key,String value) {return build(key,null,value);}
 	
 	public static Remark build(String key,Integer version,String value)
