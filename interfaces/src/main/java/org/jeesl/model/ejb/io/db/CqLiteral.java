b6c003a08869c5ac527eb30d89377185f0b82c33
Thorsten-K <t.kisner@web.de>
2024-03-30 17:12:31 +0100

    Interface JeeslCqLiteral added

--- interfaces/src/main/java/org/jeesl/model/ejb/io/db/CqLiteral.java
+++ interfaces/src/main/java/org/jeesl/model/ejb/io/db/CqLiteral.java
@@ -2,15 +2,13 @@ package org.jeesl.model.ejb.io.db;
 
 import java.io.Serializable;
 
-public class CqLiteral implements Serializable
+public class CqLiteral implements JeeslCqLiteral
 {
 	private static final long serialVersionUID = 1L;
-
-	public enum Type {STARTS,CONTAINS}
 	
-	private final Type type; public final Type getType() {return type;}
-	private final String literal; public String getLiteral() {return literal;}
-	private final String path; public String getPath() {return path;}
+	private final Type type; @Override public Type getType() {return type;}
+	private final String literal; @Override public String getLiteral() {return literal;}
+	private final String path; @Override public String getPath() {return path;}
 
 	public static CqLiteral starts(String literal, String path) {return new CqLiteral(Type.STARTS,literal,path);}
 	public static CqLiteral contains(String literal, String path) {return new CqLiteral(Type.CONTAINS,literal,path);}
