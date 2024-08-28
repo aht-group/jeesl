4833da3e1dc304dcf7dd20937ba3d5fa0c274e0c
manandhar-aht <sachin.manandhar@aht-group.com>
2024-08-28 08:27:02 +0200

    fix API field change in Acled data

--- util/src/main/java/org/jeesl/factory/json/extern/aceld/JsonCountryFactory.java
+++ util/src/main/java/org/jeesl/factory/json/extern/aceld/JsonCountryFactory.java
@@ -1,7 +1,7 @@
 package org.jeesl.factory.json.extern.aceld;
 
-import org.jeesl.model.json.ssi.acled.JsonAcledData;
 import org.jeesl.model.json.ssi.acled.JsonAcledCountry;
+import org.jeesl.model.json.ssi.acled.JsonAcledData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,17 +10,17 @@ public class JsonCountryFactory
 	final static Logger logger = LoggerFactory.getLogger(JsonCountryFactory.class);
 
 	public static JsonAcledCountry build() {return new JsonAcledCountry();}
-		
+
     public static JsonAcledCountry build(JsonAcledData data)
     {
     	JsonAcledCountry json = build();
     	json.setId(Long.valueOf(data.getIso()));
     	json.setName(data.getCountry());
     	json.setEvents(data.getCount());
-    	json.setIso3(data.getIso3());
+    	json.setIso3(data.getIso());
     	return json;
     }
-    
+
     public static String toSsiCode(JsonAcledCountry json)
     {
     	return json.getId().toString();
