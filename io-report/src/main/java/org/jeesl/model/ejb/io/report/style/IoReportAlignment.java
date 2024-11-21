084887a540fb602a4f50707162bff4f229d3bdab
Thorsten-K <t.kisner@web.de>
2024-11-21 11:26:45 +0100

    io/report activated

---
+++ io-report/src/main/java/org/jeesl/model/ejb/io/report/style/IoReportAlignment.java
@@ -0,0 +1,34 @@
+package org.jeesl.model.ejb.io.report.style;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.jeesl.interfaces.model.io.report.style.JeeslReportAlignment;
+import org.jeesl.interfaces.qualifier.er.EjbErNode;
+import org.jeesl.model.ejb.io.graphic.core.IoGraphic;
+import org.jeesl.model.ejb.io.locale.IoDescription;
+import org.jeesl.model.ejb.io.locale.IoLang;
+import org.jeesl.model.ejb.io.locale.IoStatus;
+
+@Entity
+@DiscriminatorValue("ioReportAlingment")
+@EjbErNode(name="Alignment",category="system",subset="report",level=3)
+
+public class IoReportAlignment extends IoStatus implements JeeslReportAlignment<IoLang,IoDescription,IoReportAlignment,IoGraphic>
+{
+	public static final long serialVersionUID=1;
+	
+	@Override public List<String> getFixedCodes()
+	{
+		List<String> fixed = new ArrayList<String>();
+		for(JeeslReportAlignment.Code c : JeeslReportAlignment.Code.values()){fixed.add(c.toString());}
+		return fixed;
+	}
+	
+	@Override public boolean equals(Object object) {return (object instanceof IoReportAlignment) ? id == ((IoReportAlignment) object).getId() : (object == this);}
+	@Override public int hashCode(){return new HashCodeBuilder(17,37).append(id).toHashCode();}
+}
