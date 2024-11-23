3718c3e88cc733486eced801e960351f45da1f51
Thorsten-K <t.kisner@web.de>
2024-11-23 07:20:16 +0100

    azureUser improved

--- xml/src/main/java/org/jeesl/model/json/system/security/azure/JsonAzureUser.java
+++ xml/src/main/java/org/jeesl/model/json/system/security/azure/JsonAzureUser.java
@@ -14,17 +14,42 @@ public class JsonAzureUser implements Serializable
 
 	public JsonAzureUser() {}
 	
-	@JsonProperty("guid")
-	private String guid;
-	public String getGuid() {return guid;}
-	public void setGuid(String guid) {this.guid = guid;}
-
-	@JsonProperty("email")
+	@JsonProperty("PrimarySmtpAddress")
 	private String email;
 	public String getEmail() {return email;}
 	public void setEmail(String email) {this.email = email;}
 	
-    @JsonProperty("SurName")
+	@JsonProperty("Guid")
+	private String guid;
+	public String getGuid() {return guid;}
+	public void setGuid(String guid) {this.guid = guid;}
+	
+//	@JsonProperty("DatabaseGuid")
+//	private String databaseGuid;
+//    public String getDatabaseGuid() {return databaseGuid;}
+//	public void setDatabaseGuid(String databaseGuid) {this.databaseGuid = databaseGuid;}
+//	
+//	@JsonProperty("ExchangeGuid")
+//	private String exchangeGuid;
+//	public String getExchangeGuid() {return exchangeGuid;}
+//	public void setExchangeGuid(String exchangeGuid) {this.exchangeGuid = exchangeGuid;}
+
+	@JsonProperty("DisplayName")
+    protected String displayName;
+	public String getDisplayName() {return displayName;}
+	public void setDisplayName(String displayName) {this.displayName = displayName;}
+	
+	@JsonProperty("IsShared")
+	private Boolean  shared;
+	public Boolean getShared() {return shared;}
+	public void setShared(Boolean shared) {this.shared = shared;}
+
+	@JsonProperty("RecipientType")
+    protected String type;
+	public String getType() {return type;}
+	public void setType(String type) {this.type = type;}
+
+	@JsonProperty("SurName")
     protected String firstName;
     public String getFirstName() {return firstName;}
     public void setFirstName(String value) {this.firstName = value;}
