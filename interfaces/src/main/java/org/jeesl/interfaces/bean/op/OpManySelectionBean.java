4cab8aa264c98d9f5cc3721ca1d997f97c8eb75b
Thorsten-K <t.kisner@web.de>
2025-01-02 12:54:21 +0100

    Factory updated

--- interfaces/src/main/java/org/jeesl/interfaces/bean/op/OpManySelectionBean.java
+++ interfaces/src/main/java/org/jeesl/interfaces/bean/op/OpManySelectionBean.java
@@ -1,11 +1,13 @@
 package org.jeesl.interfaces.bean.op;
 
+import java.io.Serializable;
+
 import org.jeesl.exception.ejb.JeeslConstraintViolationException;
 import org.jeesl.exception.ejb.JeeslLockingException;
 import org.jeesl.interfaces.controller.handler.op.OpSelectionHandler;
 import org.jeesl.interfaces.model.with.primitive.number.EjbWithId;
 
-public interface OpManySelectionBean
+public interface OpManySelectionBean extends Serializable
 {
 //	void x();
 	void callbackOpSelection(OpSelectionHandler handler, EjbWithId ejb) throws JeeslLockingException, JeeslConstraintViolationException;
