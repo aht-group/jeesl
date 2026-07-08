4480ed782a0f36c6ecb5b901ddd38de251756ead
Thorsten-K <t.kisner@web.de>
2026-07-08 07:40:05 +0200

    LatestFileFactory improved

--- util/src/main/java/org/jeesl/factory/java/io/fr/JeeslLatestFileFactory.java
+++ util/src/main/java/org/jeesl/factory/java/io/fr/JeeslLatestFileFactory.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
@@ -25,15 +26,15 @@ public class JeeslLatestFileFactory
 		{
 			for(File f : pDir.toFile().listFiles())
 			{
-				logger.trace(f.toString());
+				logger.info(f.toString());
 			}
 			
 			Stream<Path> stream = Files.list(pDir);
 			Path latest = stream
 	                .filter(Files::isRegularFile)
-	                .map(p -> new NamedFile(dtf,p))
-	                .filter(nf -> nf.ts != null)               // only valid pattern
-	                .max(Comparator.comparing(nf -> nf.ts))    // latest pattern
+	                .map(p -> new NamedDateTimeFile(dtf,p))
+	                .filter(nf -> nf.ldt != null)               // only valid pattern
+	                .max(Comparator.comparing(nf -> nf.ldt))    // latest pattern
 	                .map(nf -> nf.path)
 	                .orElse(null);
 			stream.close();
@@ -43,20 +44,60 @@ public class JeeslLatestFileFactory
 		}
 		catch (IOException e) {e.printStackTrace();}
 	    return null;
-	 }
+	}
+	
+	
 
-    private static class NamedFile
+    private static class NamedDateTimeFile
     {
         final Path path;
-        LocalDateTime ts;
+        LocalDateTime ldt;
         
-        NamedFile(DateTimeFormatter dtf, Path path)
+        NamedDateTimeFile(DateTimeFormatter dtf, Path path)
         {
             this.path = path;
-            try {this.ts = LocalDateTime.parse(path.getFileName().toString(),dtf);}
-            catch (Exception e) {this.ts=null;}
+            try {this.ldt = LocalDateTime.parse(path.getFileName().toString(),dtf);}
+            catch (Exception e) {this.ldt=null;}
         }
+    }
+    
+    public static Path pathByDate(Path pDir, DateTimeFormatter dtf)
+	{
+		logger.trace(pDir.toString());
+		try
+		{
+			for(File f : pDir.toFile().listFiles())
+			{
+				logger.info(f.toString());
+			}
+			
+			Stream<Path> stream = Files.list(pDir);
+			Path latest = stream
+	                .filter(Files::isRegularFile)
+	                .map(p -> new NamedDateFile(dtf,p))
+	                .filter(nf -> nf.ld != null)               // only valid pattern
+	                .max(Comparator.comparing(nf -> nf.ld))    // latest pattern
+	                .map(nf -> nf.path)
+	                .orElse(null);
+			stream.close();
+			
+			if(Objects.isNull(latest)) {logger.warn("No latest file in "+pDir.toString()); LoggedExit.exit(true);}
+			return latest;
+		}
+		catch (IOException e) {e.printStackTrace();}
+	    return null;
+	}
+    
+    private static class NamedDateFile
+    {
+        final Path path;
+        LocalDate ld;
         
-       
+        NamedDateFile(DateTimeFormatter dtf, Path path)
+        {
+            this.path = path;
+            try {this.ld = LocalDate.parse(path.getFileName().toString(),dtf);}
+            catch (Exception e) {this.ld=null;}
+        }
     }
 }
